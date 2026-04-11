9f3fd296e178690a1816bce55ff5444aaedfa8fc
Al-mizan <mdalmizanakon@gmail.com>
2026-04-11 17:25:43 +0600

    done prescription and review system

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { AdminRoutes } from "../module/admin/admin.route";
+import { AppointmentRoutes } from "../module/appointment/appointment.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { DoctorRoutes } from "../module/doctor/doctor.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
+import { PatientRoutes } from "../module/patient/patient.route";
+import { PrescriptionRoutes } from "../module/prescription/prescription.route";
+import { ReviewRoutes } from "../module/review/review.route";
 import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { UserRoutes } from "../module/user/user.route";
-import { SpecialtyRoutes } from "../module/speciality/speciality.routes";
-import { AppointmentRoutes } from "../module/appointment/appointment.route";
-import { PatientRoutes } from "../module/patient/patient.route";
+import { SpecialtyRoutes } from "../module/specialty/speciality.routes";
 
 const router = Router();
 
@@ -20,6 +22,8 @@ router.use("/admins", AdminRoutes)
 router.use("/schedules", scheduleRoutes)
 router.use("/doctor-schedules", DoctorScheduleRoutes)
 router.use("/appointments", AppointmentRoutes)
+router.use("/prescriptions", PrescriptionRoutes)
+router.use("/reviews", ReviewRoutes)
 
 
 export const IndexRoutes = router;
